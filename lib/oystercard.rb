f9d032793d6d959bd4437a41f699a6d1099d7346
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-04-27 15:37:53 +0100

    Added station argument and entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
   attr_reader :balance
+  attr_reader :entry_station
 
   MAX_BALANCE = 90
   MIN_BALANCE = 1
 
   def initialize
     @balance = 0
-    @journey_checker = "touched_out"
+    # @journey_checker = "touched_out"
+    @entry_station = nil
   end
 
   def add_money(money)
@@ -14,19 +16,23 @@ class Oystercard
     @balance += money
   end
 
-  def touch_in
+  def touch_in(station)
     fail "Insufficient funds" if @balance < MIN_BALANCE
-    @journey_checker = "touched_in"
+    @entry_station = station
+    # @journey_checker = "touched_in"
   end
 
   def touch_out
     fail "You're not touched in" unless on_journey?
     deduct_money(MIN_BALANCE)
-    @journey_checker = "touched_out"
+    @entry_station = nil
+    # @journey_checker = "touched_out"
   end
 
   def on_journey?
-    @journey_checker == "touched_in"
+    # @journey_checker == "touched_in"
+    # !!entry_station is the equivilent of below
+    @entry_station == nil ? false : true
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,6 +8,8 @@ describe Oystercard do
 
   #plan to remove this test when refactoring once we have respond to money with 1 argument working
 
+  let(:station) { double :station }
+
   describe '#add money' do
     it 'responds to add_money with 1 argument' do
       expect(subject).to respond_to(:add_money).with(1).argument
@@ -38,17 +40,17 @@ describe Oystercard do
   #   expect(subject).to respond_to(:deduct_money).with(1).argument
   # end
 
-  it 'can touch_in' do
-    subject.add_money(Oystercard::MIN_BALANCE)
-    expect(subject.touch_in).to eq "touched_in"
-  end
+  # it 'can touch_in' do
+  #   subject.add_money(Oystercard::MIN_BALANCE)
+  #   expect(subject.touch_in(station)).to eq "touched_in"
+  # end
 
   describe '#can_touch_out' do
-    it 'can_touch_out' do
-      subject.add_money(Oystercard::MIN_BALANCE)
-      subject.touch_in
-      expect(subject.touch_out).to eq "touched_out"
-    end
+    # it 'can_touch_out' do
+    #   subject.add_money(Oystercard::MIN_BALANCE)
+    #   subject.touch_in(station)
+    #   expect(subject.touch_out).to eq "touched_out"
+    # end
     it 'can only touch_out if touched in' do
       expect { subject.touch_out }.to raise_error "You're not touched in"
     end
@@ -56,23 +58,23 @@ describe Oystercard do
 
   it "tells user if they are on a journey" do
     subject.add_money(Oystercard::MIN_BALANCE)
-    subject.touch_in
+    subject.touch_in(station)
     expect(subject.on_journey?).to eq true
   end
 
   it "refuses entry unless minimum balance is #{Oystercard::MIN_BALANCE}" do
-    expect { subject.touch_in }.to raise_error "Insufficient funds"
+    expect { subject.touch_in(station) }.to raise_error "Insufficient funds"
   end
 
   it "removes fare from balance" do
     subject.add_money(Oystercard::MIN_BALANCE)
-    subject.touch_in
+    subject.touch_in(station)
     expect{ subject.touch_out }.to change {subject.balance}.by(-Oystercard::MIN_BALANCE)
   end
 
   it 'remembers entry station' do
     subject.add_money(1)
-    subject.touch_in
+    subject.touch_in(station)
     expect(subject.entry_station).not_to eq nil
   end
 end
