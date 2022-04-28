d50c4e5a2e8f13f494447387cc0626372b6e4fbe
Luke Storey <lukemstorey@gmail.com>
2022-04-28 16:12:11 +0100

    Step 13 - Test for station to have zone

--- lib/station.rb
+++ lib/station.rb
@@ -1,2 +1,7 @@
 class Station
+  attr_reader :zone
+
+  def initialize(zone_number)
+    @zone = 1
+  end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,16 @@
 require 'station'
 
 describe Station do
-  
+
+  describe '#zone' do
+    it 'should have a zone' do
+      expect(Station).to respond_to(:new).with(1).argument
+    end
+
+    it 'should return zone' do
+      zone = 1
+      station = Station.new(zone)
+      expect(station.zone).to eq(zone)
+    end
+  end
 end
